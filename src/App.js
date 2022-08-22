4412128dd04084b975c880969b22a5f1f4e308cd
JeonIJin <pongdang0121@gmail.com>
2022-08-22 14:25:23 +0900

    feat: 프로토타입 작성 (#10)
    * Docs/readme update (#6)
    
    * feat: initial commit
    
    * docs: README.md 업데이트
    
    * feat: todoData
    
    * feat: todo 프로토타입 작성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,34 @@
+import React, { useEffect, useState } from 'react';
+
 function App() {
-  return <h1>잘 되나용</h1>;
+  const [todoList, setTodoList] = useState([]);
+
+  useEffect(() => {
+    const todoListDataURI = '/todoData.json';
+    fetch(todoListDataURI)
+      .then((res) => res.json())
+      .then((data) => {
+        setTodoList(data.todoData);
+      });
+  }, []);
+
+  return (
+    <section>
+      <h1>To do</h1>
+      <input />
+      <button>추가</button>
+      <ul>
+        {todoList.map((todo) => (
+          <React.Fragment key={todo.id}>
+            <li>
+              <input type="checkbox" />
+              {todo.content}
+            </li>
+          </React.Fragment>
+        ))}
+      </ul>
+    </section>
+  );
 }
 
 export default App;
